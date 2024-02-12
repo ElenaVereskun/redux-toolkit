96b1957e71a1419eff49c25e63556e965218ec5e
ElenaVereskun <lena.vereskun@yandex.ru>
2024-02-12 16:30:10 +0300

    install dep

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+function App() {
+  return <div>App</div>
+}
+12
+
+export default App
---
+++ src/app/store.ts
@@ -0,0 +1,20 @@
+import type { Action, ThunkAction } from "@reduxjs/toolkit"
+import { combineSlices, configureStore } from "@reduxjs/toolkit"
+import { setupListeners } from "@reduxjs/toolkit/query"
+
+export const store = configureStore({
+  reducer:{
+
+  }
+})
+
+
+export type AppDispatch = typeof store.dispatch
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>
